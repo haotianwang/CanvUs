8cfef9f8591c24f678dd464134bef758c7b3f630
haotian wang <haotianwang92@gmail.com>
2013-02-25 19:32:33 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CanvUs::Application.config.secret_token = 'e9c800e017deb8f165902829a51c2dbb4fd528ed6a64ca9028cdbdc429a3766c0f9124fe68431952de2e024dcc856afbe3bbdd3ceb0a2504b35fb31750847c35'
