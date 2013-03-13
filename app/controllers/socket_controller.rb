e2f4d07960817196080ba83bde260e16d6dd3a48
Perlin Lim <perlin888@gmail.com>
2013-03-13 00:10:35 -0700

    Add SocketController Methods

--- app/controllers/socket_controller.rb
+++ app/controllers/socket_controller.rb
@@ -1,14 +1,19 @@
-class SocketController < WebsocketRails::BaseController
-	def initialize_session
+class SocketsController < WebsocketRails::BaseController
+	
+  def initialize_session
+    controller_store[:action_count] = 0
 	end
 	
 	def get_action_handler
-		send_message :get_action, "got to the get action method of controller", :namespace => 'socket'
+    Action.storeAction(message['action'], message['canvasID'])
+    controller_store[:action_count] += 1
+		broadcast_message :get_action, message['action'], :namespace => 'socket'
 	end
 
 	def send_init_img
-        puts "test test test test test test before"
-		send_message :get_init_img, "got to the init img method of controller", :namespace => 'socket'
-		puts "test test test test test test"
+    bitmap = Bitmap.getBitmap(message['canvasID'])
+    actions = Action.getActions(message['canvasID'])
+		broadcast_message :get_init_img, {user: message['user'], bitmap: bitmap, actions: actions}, :namespace => 'socket'
 	end
+  # Why use WebSockets for send_init_img? We can just use a regular HTTP request.
 end
