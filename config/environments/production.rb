5294f5aa88053200a161de465e19cb30613af096
Andy Chu <saasbook@saasbook.(none)>
2013-03-13 23:42:53 -0700

    moved .gif and .png files in vendor/assets/javascript/jscolor to vendor/assets/images/jscolor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ CanvUs::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-   config.assets.precompile += %w( updateModule.js tools.js jscolor/jscolor.js base64.js canvas2Image.js draw.css )
+   config.assets.precompile += %w( updateModule.js tools.js drawModule.js jscolor/jscolor.js jscolor/* base64.js canvas2Image.js draw.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
