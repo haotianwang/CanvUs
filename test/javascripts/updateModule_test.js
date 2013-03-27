4c6968e693bed0b24ed6da343d68e65608027304
haotian wang <haotianwang92@gmail.com>
2013-03-27 15:56:43 -0700

    changes to updateModule test

--- test/javascripts/updateModule_test.js
+++ test/javascripts/updateModule_test.js
@@ -1,27 +1,51 @@
 //= require updateModule
 //= require sinon-1.6.0.js
 
+function mockWebSocketRails() {
+    this.WebSocketRailsAPI = {   
+                                trigger: function (arg1, arg2) {},
+                                bind: function (arg1, arg2) {} 
+                             };
+                                
+    this.mock = sinon.mock(this.WebSocketRailsAPI);
+
+    this.fakeWebSocketRailsConstructor = sinon.stub();
+	this.fakeWebSocketRailsConstructor.returns(this.WebSocketRailsAPI);
+}
+
+function mockUIModule() {
+
+}
+
 module( "module A", {
 	setup: function() {
         this.WebSocketRailsAPI = {   
                                     trigger: function (arg1, arg2) {},
                                     bind: function (arg1, arg2) {} 
                                 };
-
+                                
         this.mock = sinon.mock(this.WebSocketRailsAPI);
-        this.mock.expects("trigger").atLeast(0).returns();
-        this.mock.expects("bind").atLeast(1).returns();
 
     	this.fakeWebSocketRailsConstructor = sinon.stub();
 		this.fakeWebSocketRailsConstructor.returns(this.WebSocketRailsAPI);
 	},
+    
 	teardown: function() {
 		this.WebSocketRailsAPI = null;
         this.mock = null;
+        this.fakeWebSocketsRailsConstructor = null;
 	}
 });
 
 test('initialize updateModule', function() {
+    var testUpdateModule = instantiateUpdateModule(this.fakeWebSocketRailsConstructor);
+    ok(testUpdateModule != null, "initialization worked as expected");
+    this.mock.verify();
+});
+
+test('initialize updateModule', function() {
+    this.mock.expects("trigger").atLeast(0).returns();
+    this.mock.expects("bind").atLeast(1).returns();
     var testUpdateModule = instantiateUpdateModule(this.fakeWebSocketRailsConstructor);
     ok(testUpdateModule != null, "initialization worked as expected");
     this.mock.verify();
