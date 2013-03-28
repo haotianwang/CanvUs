7e5d9503fc98b9860f14db5575c75929352f36cc
haotianwang <haotianwang92@gmail.com>
2013-03-27 20:08:13 -0700

    completed unit tests for iteration1's updateModule. added updateModule.setDrawAPI() and updateModule.setSocketClass() to make code more modularized.

--- app/assets/javascripts/updateModule.js
+++ app/assets/javascripts/updateModule.js
@@ -1,94 +1,129 @@
 /*
-The UpdateModule class will incorporate all of the functionality of the Update Module.
-After instantiating the class with new UpdateModule(), the function names directly map
-to the functionality that the Update Module must have.
+The UpdateModule class will incorporate all of the functionality of the Update Module. 
+the function names directly map to the functionality that the Update Module must have.
 - UpdateModule.sendAction(DrawActionType, startx, starty, endx, endy, color, strokeWidth)
-	- DrawActionTypes supported (string): "rect", "circle", "line", "clear"
-	- "clear" DrawActionType ignores other arguments
-	- "line" DrawActionType also covers the "pencil" tool, since the pencil just draws a bunch of lines
+    - DrawActionTypes supported (string): "rect", "circle", "line", "clear"
+    - "clear" DrawActionType ignores other arguments
+    - "line" DrawActionType also covers the "pencil" tool, since the pencil just draws a bunch of lines
 - UpdateModule.handleGetAction(data)
 - updateModule.getInitImg()
 
-*/
-
-/*
-$(document).ready(function() { 
-	var updateModule = instantiateUpdateModule();
-	updateModule.getInitImg();
-	updateModule.sendAction("action testing testing 123");
-	console.log(document.domain + document.port);
-});
+INITIALIZATION:
+    instantiateUpdateModule(socketClass);
 */
 
 function instantiateUpdateModule(socketClass) {
-	var module = new UpdateModule(socketClass);
-	module.dispatcher.bind('socket.get_init_img', function(data){module.getInitImgHandler(data)});
-	module.dispatcher.bind('socket.get_action', function(data){module.handleGetAction(data)});
-	return module;
+    var module = new UpdateModule();
+    module.setSocketClass(socketClass);
+    module.setDrawAPI(getDrawAPI());
+    module.initialize();
+    return module;
 }
 
-function UpdateModule(socketClass) {
-    this.url = document.URL.split( '/' )[2] + "/websocket";
-	console.log("socket at " + this.url);
-	this.dispatcher = new socketClass(this.url);
+function UpdateModule() {
+    this.socketClass = null;
+    this.dispatcher = null;
+    this.DrawAPI = null;
+
+    this.initialize = function() {
+        this.url = document.URL.split( '/' )[2] + "/websocket";
+        console.log("socket at " + this.url);
+        this.dispatcher = new this.socketClass(this.url);
+        var module = this;
+        this.dispatcher.bind('socket.get_init_img', function(data) {module.getInitImgHandler(data)});
+        this.dispatcher.bind('socket.get_action', function(data) {module.handleGetAction(data)});
+    }
+
+    this.setSocketClass = function(socketClass) {
+        this.socketClass = socketClass;
+    }
+
+    this.setDrawAPI = function (API) {
+        this.DrawAPI = API;
+    }
 
     this.sendAction = function (drawActionType, startx, starty, endx, endy, color, strokeWidth) {
-		var myJson = {"action": drawActionType, 
-					"startx": startx, 
-					"starty": starty, 
-					"endx": endx, 
-					"endy": endy,
-					"color": color,
-					"strokeWidth": strokeWidth }
-		console.log("sending action..." + JSON.stringify(myJson));
+        var myJson = {"action": drawActionType, 
+                    "startx": startx, 
+                    "starty": starty, 
+                    "endx": endx, 
+                    "endy": endy,
+                    "color": color,
+                    "strokeWidth": strokeWidth }
+        console.log("sending action..." + JSON.stringify(myJson));
         this.dispatcher.trigger('socket.send_action', JSON.stringify(myJson));
     };
 
     this.handleGetAction = function (data) {
-		console.log("got action! it's: " + data);
-		myJson = JSON.parse(data);
-		this.invokeDrawingModule(myJson.action, myJson.startx, myJson.starty, myJson.endx, myJson.endy, myJson.color, myJson.strokeWidth);
+        console.log("got action! it's: " + data);
+        myJson = JSON.parse(data);
+        this.invokeDrawingModule(myJson.action, myJson.startx, myJson.starty, myJson.endx, myJson.endy, myJson.color, myJson.strokeWidth);
     };
 
-	this.invokeDrawingModule = function (action, startx, starty, endx, endy, color, strokeWidth) {
+    this.invokeDrawingModule = function (action, startx, starty, endx, endy, color, strokeWidth) {
 
-		switch(action) {
-			case "line":
-				drawLine(dispCtx, startx, starty, endx, endy, color, strokeWidth);
-				break;
-			case "clear":			
-				clearCanvas(dispCtx);
-  				break;
-			case "rectangle":
-				drawRectangle(dispCtx, startx, starty, endx, endy, color, strokeWidth);
-				break;
-			case "circle":
-				drawCircle(dispCtx, startx, starty, endx, endy, color, strokeWidth);
-				break;
-			default:
-  				console.log("invokeDrawingModule failed due to unknown action: " + action);
-		}
-	};
+        switch(action) {
+            case "line":
+                this.DrawAPI.drawLine(dispCtx, startx, starty, endx, endy, color, strokeWidth);
+                break;
+            case "clear":           
+                this.DrawAPI.clearCanvas(dispCtx);
+                break;
+            case "rectangle":
+                this.DrawAPI.drawRectangle(dispCtx, startx, starty, endx, endy, color, strokeWidth);
+                break;
+            case "circle":
+                this.DrawAPI.drawCircle(dispCtx, startx, starty, endx, endy, color, strokeWidth);
+                break;
+            default:
+                console.log("invokeDrawingModule failed due to unknown action: " + action);
+        }
+    };
 
     this.getInitImg = function () {
-		console.log("getting initial image...");
-		this.dispatcher.trigger('socket.send_init_img', "");
+        console.log("getting initial image...");
+        this.dispatcher.trigger('socket.send_init_img', "");
     };
 
-	this.getInitImgHandler = function (data) {
-		console.log("got image, raw data is " + data);
-		var myJson = JSON.parse(data);
-		console.log("got image. it's: " + JSON.stringify(myJson));
-		if (myJson.bitmap != "") {
-			
-		}
-		if (myJson.actions != "") {
-			var actions = myJson.actions.split(", ");
-			for (var i = 0; i < actions.length; i++) {
-				var thisAction = JSON.parse(actions[i]);
-				console.log(JSON.stringify(thisAction));
-				this.invokeDrawingModule(thisAction.action, thisAction.startx, thisAction.starty, thisAction.endx, thisAction.endy, thisAction.color, thisAction.strokeWidth);
-			}
-		}
-	};
+    this.getInitImgHandler = function (data) {
+        console.log("got image, raw data is " + data);
+        var myJson = JSON.parse(data);
+        console.log("got image. it's: " + JSON.stringify(myJson));
+        if (myJson.bitmap != "") {
+            this.DrawAPI.drawBitmap(dispCtx, myJson.bitmap);
+        }
+        if (myJson.actions != "") {
+            var actions = myJson.actions.split(", ");
+            for (var i = 0; i < actions.length; i++) {
+                var thisAction = JSON.parse(actions[i]);
+                console.log(JSON.stringify(thisAction));
+                this.invokeDrawingModule(thisAction.action, thisAction.startx, thisAction.starty, thisAction.endx, thisAction.endy, thisAction.color, thisAction.strokeWidth);
+            }
+        }
+    };
+}
+
+function getDrawAPI() {
+    API = {
+        drawLine: function(dispCtx, startx, starty, endx, endy, color, strokeWidth) { 
+            drawLine(dispCtx, startx, starty, endx, endy, color, strokeWidth); 
+        },
+            
+        clearCanvas: function(dispCtx) { 
+            clearCanvas(dispCtx); 
+        },
+
+        drawRectangle: function(dispCtx, startx, starty, endx, endy, color, strokeWidth) { 
+            drawRectangle(dispCtx, startx, starty, endx, endy, color, strokeWidth); 
+        },
+
+        drawCircle: function(dispCtx, startx, starty, endx, endy, color, strokeWidth) { 
+            drawCircle(dispCtx, startx, starty, endx, endy, color, strokeWidth); 
+        },
+
+        drawBitmap: function(dispCtx, bitmap) {
+            drawBitmap(dispCtx, bitmap);
+        }
+    }
+    return API;
 }
--- test/javascripts/updateModule_test.js
+++ test/javascripts/updateModule_test.js
@@ -1,52 +1,180 @@
 //= require updateModule
 //= require sinon-1.6.0.js
+//= require uiModule
 
+// returns [api, mock, constructor]
 function mockWebSocketRails() {
-    this.WebSocketRailsAPI = {   
+    WebSocketRailsAPI = {   
                                 trigger: function (arg1, arg2) {},
                                 bind: function (arg1, arg2) {} 
                              };
                                 
-    this.mock = sinon.mock(this.WebSocketRailsAPI);
+    mock = sinon.mock(WebSocketRailsAPI);
 
-    this.fakeWebSocketRailsConstructor = sinon.stub();
-	this.fakeWebSocketRailsConstructor.returns(this.WebSocketRailsAPI);
+    fakeWebSocketRailsConstructor = sinon.stub();
+	fakeWebSocketRailsConstructor.returns(WebSocketRailsAPI);
+
+    return [WebSocketRailsAPI, mock, fakeWebSocketRailsConstructor];
 }
 
-function mockUIModule() {
+// returns [api, mock]
+function mockDrawModule() {
+    fakeDrawAPI = getDrawAPI();
+    mock = sinon.mock(fakeDrawAPI);
 
+    return [fakeDrawAPI, mock];
+}
+
+function createStubAction(actionType) {
+    return JSON.stringify({"action": actionType, "startx": 0, "starty": 0, "endx": 0, "endy": 0, "color": "0", "strokeWidth": 0});
 }
 
 module( "updateModule tests", {
 	setup: function() {
-        this.WebSocketRailsAPI = {   
-                                    trigger: function (arg1, arg2) {},
-                                    bind: function (arg1, arg2) {} 
-                                };
-                                
-        this.mock = sinon.mock(this.WebSocketRailsAPI);
+        var mockedWebSocketRailsPackage = mockWebSocketRails();
+        this.WebSocketRailsAPI = mockedWebSocketRailsPackage[0];
+        this.WebSocketRailsMock = mockedWebSocketRailsPackage[1];
+        this.WebSocketRailsConstructor = mockedWebSocketRailsPackage[2];
 
-    	this.fakeWebSocketRailsConstructor = sinon.stub();
-		this.fakeWebSocketRailsConstructor.returns(this.WebSocketRailsAPI);
+        var mockedDrawModulePackage = mockDrawModule();
+        this.fakeDrawModule= mockedDrawModulePackage[0];
+        this.fakeDrawModuleMock = mockedDrawModulePackage[1];
+
+        this.testUpdateModule = instantiateUpdateModule(this.WebSocketRailsConstructor);
+        this.testUpdateModule.setDrawAPI(this.fakeDrawModule);
 	},
     
 	teardown: function() {
-		this.WebSocketRailsAPI = null;
-        this.mock = null;
-        this.fakeWebSocketsRailsConstructor = null;
-	}
+        this.WebSocketRailsMock.restore();
+
+        this.fakeDrawModuleMock.restore();
+
+        this.WebSocketRailsAPI = null;
+        this.WebSocketRailsMock = null;
+        this.WebSocketRailsConstructor = null;
+
+        this.fakeDrawModule= null;
+        this.fakeDrawModuleMock = null;
+
+        this.testUpdateModule = null
+;	}
 });
 
-test('initialize updateModule', function() {
-    var testUpdateModule = instantiateUpdateModule(this.fakeWebSocketRailsConstructor);
+test('instantiateUpdateModule', function() {
+    // there should be 1 bind for every type of client-server API action
+    this.WebSocketRailsMock.expects("bind").exactly(2);
+    this.WebSocketRailsMock.expects("trigger").exactly(0);
+
+    // initialize, just to make sure it works
+    var testUpdateModule = instantiateUpdateModule(this.WebSocketRailsConstructor);
     ok(testUpdateModule != null, "initialization worked as expected");
-    this.mock.verify();
+
+    this.WebSocketRailsMock.verify();
 });
 
-test('initialize updateModule', function() {
-    this.mock.expects("trigger").atLeast(0).returns();
-    this.mock.expects("bind").atLeast(1).returns();
-    var testUpdateModule = instantiateUpdateModule(this.fakeWebSocketRailsConstructor);
-    ok(testUpdateModule != null, "initialization worked as expected");
-    this.mock.verify();
+test('updateModule.setSocketClass', function() {
+    var randomSocket = "random string";
+    this.testUpdateModule.setSocketClass(randomSocket);
+
+    equal(this.testUpdateModule.socketClass, randomSocket, "updateModule.setSocketClass correctly set the class");
+});
+
+test('updateModule.sendAction', function() {
+
+    // make an expected JSON out of the args to be passed to sendAction
+    var args = ["action", "startx", "starty", "endx", "endy", "color", "strokeWidth"];
+    var expectedAction = {};
+    for (var i = 0; i < args.length; i++) {
+        expectedAction[args[i]] = args[i];
+    }
+    expectedAction = JSON.stringify(expectedAction);
+
+    // expect trigger to be called once (for the send action) with the right type of event, and the expected action string
+    this.WebSocketRailsMock.expects("trigger").withArgs("socket.send_action", expectedAction).exactly(1);
+
+    this.testUpdateModule.sendAction(args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+
+    ok(this.WebSocketRailsMock.verify(), "verified that sendAction sent the right kind of action");
+});
+
+test('updateModule.handleGetAction', function() {
+    // make the action to send to updateModule
+    var action = createStubAction("clear");
+    // this action should invoke clearCanvas exactly once
+    this.fakeDrawModuleMock.expects("clearCanvas").returns("").exactly(1);
+
+    this.testUpdateModule.handleGetAction(action);
+
+    ok(this.fakeDrawModuleMock.verify(), "verified that getting a clear action invokes the clearCanvas DrawAPI method");
+});
+
+test('updateModule.invokeDrawingModule all draw methods', function() {
+    // test that when an action in handleGetAction maps to the correct invokeDrawingModule switch case
+    var actions = ["clear", "rectangle", "circle", "line"];
+    var actionMethods = ["clearCanvas", "drawRectangle", "drawCircle", "drawLine"];
+    for (var i = 0; i < actions.length; i++) {
+        this.setup();
+
+        // make action to be sent
+        var action = createStubAction(actions[i]);
+        // expect ith DrawAPI method to be invoked
+        this.fakeDrawModuleMock.expects(actionMethods[i]).exactly(1);
+        // spy on testUpdateModule.invokeDrawingModule to make sure it's called correctly
+        var spy = sinon.spy(this.testUpdateModule, "invokeDrawingModule");
+
+        this.testUpdateModule.handleGetAction(action);
+
+        ok(this.fakeDrawModuleMock.verify(), "verified that getting a " + actions[i] + " action invokes the " + actionMethods[i] + " DrawAPI method");
+        ok(spy.callCount == 1, "invokeDrawingModule called once, as expected");
+        ok(spy.calledWith(actions[i]), "invokeDrawingModule called with \"" + actions[i] + "\" as action, as expected");
+
+        if (i < actions.length-1){
+            this.teardown();
+        }
+    }
+});
+
+test('updateModule.getInitImg', function() {
+    // make sure the method sends the correct event: socket.send_init_img
+    this.WebSocketRailsMock.expects("trigger").withArgs("socket.send_init_img", "").exactly(1);
+    this.testUpdateModule.getInitImg();
+
+    ok(this.WebSocketRailsMock.verify(), "getInitImg sent the expected socket.send_init_img event to web socket");
+});
+
+test('updateModule.getInitImgHandler no bitmap, no action', function() {
+    // send empty init package
+    var message = JSON.stringify({"bitmap": "", "actions": ""});
+    var spy = sinon.spy(this.testUpdateModule, "invokeDrawingModule");
+
+    this.testUpdateModule.getInitImgHandler(message);
+
+    ok(spy.callCount == 0, "getInitImgHandler did not invoke drawing module, as expected.");
+});
+
+test('updateModule.getInitImgHandler no bitmap, with action', function() {
+    // send init package with 2 actions
+    var message = JSON.stringify({"bitmap": "", "actions": ""+createStubAction("rectangle")+", "+createStubAction("clear")});
+
+    // expect each of the 2 actions to invoke a drawing method as well as the invokeDrawingModule method
+    var spy = sinon.spy(this.testUpdateModule, "invokeDrawingModule");
+    this.fakeDrawModuleMock.expects("drawRectangle").exactly(1);
+    this.fakeDrawModuleMock.expects("clearCanvas").exactly(1);
+
+    this.testUpdateModule.getInitImgHandler(message);
+
+    ok(spy.callCount == 2, "getInitImgHandler called invokeDrawingModule 2 times, as expected");
+    ok(this.fakeDrawModuleMock.verify(), "getInitImgHandler called the appropriate draw actions");
+});
+
+test('updateModule.getInitImgHandler with bitmap, no action', function() {
+    // send init package with 1 bitmap and 2 actions
+    var message = JSON.stringify({"bitmap": "fake bitmap", "actions": ""});
+    var spy = sinon.spy(this.testUpdateModule, "invokeDrawingModule");
+    this.fakeDrawModuleMock.expects("drawBitmap").exactly(1);
+
+    this.testUpdateModule.getInitImgHandler(message);
+
+    ok(spy.callCount == 0, "getInitImgHandler did not invokeDrawingModule, as expected");
+    ok(this.fakeDrawModuleMock.verify(), "getInitImgHandler invoked drawBitmap, as expected");
 });
