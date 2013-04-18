728f49f1fc750f1ea0cc35ce3e866761e9afd24f
Alan Huynh <alandhuynh@berkeley.edu>
2013-04-17 17:06:01 -0700

    Finish removing canvas_id usage from Canvas Model and Canvas Model tests

--- app/models/canvas.rb
+++ app/models/canvas.rb
@@ -3,27 +3,27 @@ class Canvas < ActiveRecord::Base
 
   validates :bitmap_id, :user_username, presence: true
 
-  def self.createCanvas(canvasID, bitmapID)
+  def self.createCanvas(bitmapID)
     canvas = Canvas.create(bitmap_id: bitmapID, user_username: 'mylittlelucas888', active: true)
     return canvas[:id]
   end
 
   def self.setBitmap(canvasID, bitmapID)
-    canvas = Canvas.where("canvas_id = ?", canvasID).first
+    canvas = Canvas.where("id = ?", canvasID).first
     canvas[:bitmap_id] = bitmapID
     canvas.save
   end
 
   def self.setActivity(canvasID, active)
-    canvas = Canvas.where("canvas_id = ?", canvasID).first
+    canvas = Canvas.where("id = ?", canvasID).first
     canvas[:active] = active
     canvas.save
   end
 
   # Gets the Canvas record ids for all active canvases, i.e. canvases that are currently being drawn on.
   def self.getCanvasIDs()
-    canvases = Canvas.select("canvas_id").where("active = ?", true)
-    canvases = canvases.map{|record| record[:canvas_id]}
+    canvases = Canvas.select("id").where("active = ?", true)
+    canvases = canvases.map{|record| record[:id]}
     return canvases.join("|")
   end
 end
--- test/unit/canvas_test.rb
+++ test/unit/canvas_test.rb
@@ -9,41 +9,33 @@ class CanvasTest < ActiveSupport::TestCase
     bitmap0 = Bitmap.create(bitmap: 'bitmap_0', canvas_id: 0)
     bitmap1 = Bitmap.create(bitmap: 'bitmap_1', canvas_id: 1)
     bitmap2 = Bitmap.create(bitmap: 'bitmap_2', canvas_id: 2)
-    Canvas.create(canvas_id: 0, bitmap_id: bitmap0[:id], user_username: 'King Altruism', active: true)
-    Canvas.create(canvas_id: 1, bitmap_id: bitmap1[:id], user_username: 'King Altruism', active: true)
-    Canvas.create(canvas_id: 2, bitmap_id: bitmap2[:id], user_username: 'King Altruism', active: true)
+    Canvas.create(bitmap_id: bitmap0[:id], user_username: 'King Altruism', active: true)
+    Canvas.create(bitmap_id: bitmap1[:id], user_username: 'King Altruism', active: true)
+    Canvas.create(bitmap_id: bitmap2[:id], user_username: 'King Altruism', active: true)
   end
 
   # Test that the correct validations are enforced on Canvas record creation.
   def testCanvasValidations
     numCanvases = Canvas.all.count
-    Canvas.create(bitmap_id: 1, user_username: 'HaotianWang92', active: true)
-    Canvas.create(canvas_id: 3, user_username: 'HaotianWang92', active: true)
-    Canvas.create(canvas_id: 4, bitmap_id: 2, active: true)
+    Canvas.create(user_username: 'HaotianWang92', active: true)
+    Canvas.create(bitmap_id: 2, active: true)
     assert_equal(numCanvases, Canvas.all.count, "Canvases with empty fields were incorrectly inserted")
-  end
-
-  # Test that canvas_id is actually a primary key, preventing duplicate canvas_id field values.
-  def testCreateCanvasDuplicateID
-    numCanvases = Canvas.all.count
-    assert_raise(ActiveRecord::RecordNotUnique) { Canvas.createCanvas(0, 1) }
-    assert_equal(numCanvases, Canvas.all.count, "Canvas with a duplicate id was incorrectly stored")
   end
 
   # Test that createCanvas actually inserts a new Canvas record into the database.
   def testCreateCanvas
     numCanvases = Canvas.all.count
-    Canvas.createCanvas(3, 1)
+    Canvas.createCanvas(1)
     assert_equal(numCanvases+1, Canvas.all.count, "New Canvas was not inserted into the database")
     Canvas.last.destroy
   end
 
   # Test that setBitmap actually changes the state of a record stored in the database.
   def testSetBitmap
-    canvas = Canvas.where("canvas_id = ?", 0).first
+    canvas = Canvas.where("id = ?", 1).first
     originalBitmapID = canvas[:bitmap_id]
-    Canvas.setBitmap(canvas[:canvas_id], 3)
-    canvas = Canvas.where("canvas_id = ?", 0).first
+    Canvas.setBitmap(canvas[:id], 3)
+    canvas = Canvas.where("id = ?", 1).first
     newBitmapID = canvas[:bitmap_id]
     assert_not_equal(Bitmap.where("id = ?", originalBitmapID).first, Bitmap.where("id = ?", newBitmapID).first,
                      "Canvas record's bitmap_id field was not changed")
@@ -53,10 +45,10 @@ class CanvasTest < ActiveSupport::TestCase
 
   # Test that setActivity actually changes the state of a record stored in the database.
   def testSetActivity
-    canvas = Canvas.where("canvas_id = ?", 0).first
+    canvas = Canvas.where("id = ?", 1).first
     originalActivity = canvas[:active]
-    Canvas.setActivity(canvas[:canvas_id], !originalActivity)
-    canvas = Canvas.where("canvas_id = ?", 0).first
+    Canvas.setActivity(canvas[:id], !originalActivity)
+    canvas = Canvas.where("id = ?", 1).first
     newActivity = canvas[:active]
     assert_not_equal(originalActivity, newActivity, "Canvas record's active field was not changed")
     canvas[:active] = originalActivity
@@ -66,8 +58,8 @@ class CanvasTest < ActiveSupport::TestCase
   # Test that getCanvasIDs retrieves all the Canvas record ids for all active Canvas records in the database.
   def testGetCanvasIDs
     canvasIDs = Canvas.getCanvasIDs()
-    assert_equal("0|1|2", canvasIDs, "Canvas ids were not correctly retrieved/output")
-    Canvas.create(canvas_id: 3, bitmap_id: 1, user_username: 'mylittlelucas888', active: false)
+    assert_equal("1|2|3", canvasIDs, "Canvas ids were not correctly retrieved/output")
+    Canvas.create(bitmap_id: 1, user_username: 'mylittlelucas888', active: false)
     assert_equal(canvasIDs, Canvas.getCanvasIDs, "An inactive Canvas record's id was incorrectly retrieved/output")
     Canvas.last.destroy
   end
