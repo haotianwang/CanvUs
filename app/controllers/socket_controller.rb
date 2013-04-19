62d1389bc99e8b64cac12ef9eb4fae3fd65717e1
Perlin Lim <perlin888@gmail.com>
2013-04-18 21:02:38 -0700

    Add unit tests for socket controller

--- app/controllers/socket_controller.rb
+++ app/controllers/socket_controller.rb
@@ -8,7 +8,10 @@ class SocketController < WebsocketRails::BaseController
   # Stores an action in the database and generates the first Bitmap and Canvas records for a particular canvasID
   # if they do not yet have any records in the database. Afterwards, broadcasts the action to all connected
   # clients.
-  def get_action_handler
+  def get_action_handler(test_message = nil)
+    if !test_message.nil?
+      message = test_message
+    end
     message_json = JSON.parse(message)
     action = message_json['message']
     canvas_id = message_json['canvasID']
@@ -22,7 +25,10 @@ class SocketController < WebsocketRails::BaseController
 
   # Retrieves the most recent canvas state stored in the database as well as any actions that have been done
   # since that canvas state was stored to send to a newly connected client.
-  def send_init_img
+  def send_init_img(test_message = nil)
+    if !test_message.nil?
+      message = test_message
+    end
     canvas_id = message.to_i
     bitmap_record = Bitmap.getBitmap(canvas_id)
     if bitmap_record.nil?
@@ -32,15 +38,22 @@ class SocketController < WebsocketRails::BaseController
       bitmap = bitmap_record['bitmap']
       timestamp = bitmap_record['latest_action_timestamp']
       actions = Action.getActions(canvas_id, timestamp)
+      puts timestamp
     end
     response = {bitmap: bitmap, actions: actions}.to_json
+    if !test_message.nil?
+      return response
+    end
     send_message :get_init_img, response, :namespace => 'socket'
   end
 
   # Stores an updated bitmap in the database according to the current state of the canvas a client is drawing on.
   # Broadcasts to all clients so that additional bitmaps with almost the exact same state will not be sent. Finally,
   # calls the cleanUp method to delete old data from the database.
-  def get_bitmap
+  def get_bitmap(test_message = nil)
+    if !test_message.nil?
+      message = test_message
+    end
     message_json = JSON.parse(message)
     bitmap = message_json['bitmap']
     timestamp = message_json['timestamp']
