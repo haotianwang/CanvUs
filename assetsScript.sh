5c4f9d9a23313d4decfe46933ad18862ea1dd060
Andy Chu <root@saasbook.(none)>
2013-04-20 20:50:15 -0700

    added precompile and clean assets to the script

--- assetsScript.sh
+++ assetsScript.sh
@@ -1,1 +1,5 @@
-scp -i ../../.ssh/cs169.pem -r public/assets/ ubuntu@54.241.5.2:/home/ubuntu/cs169-sp13/CanvUs2/CanvUs/public/
+#This script will precompile assets, scp the files to EC2, and then delete the precompiled files
+RAILS_ENV=production rake assets:precompile 
+scp -i ../../.ssh/cs169.pem -r public/assets/ ubuntu@54.241.5.2:/home/ubuntu/cs169-sp13/CanvUs/public/
+RAILS_ENV=production rake assets:clean
+
