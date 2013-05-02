5f8a5b9c4dbd92f6d669351dce8e2706a3a0b2e9
haotianwang <haotianwang92@gmail.com>
2013-05-02 15:34:48 -0700

    partial cache implementation

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'rufus/scheduler'  
 scheduler = Rufus::Scheduler.start_new
-#scheduler.every("60s") do
-#    Action.repeatFlush()
-#end
+scheduler.every("5s") do
+    Action.flushActions()
+    ActiveRecord::Base.connection.close
+    ActiveRecord::Base.connection_pool.clear_stale_cached_connections!
+end
